5904dd3f6745377befb15320ab31b7269d584e35
Han Nguyen <hanvnguyen94@gmail.com>
2021-07-08 11:17:38 -0400

    removed font awesome

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFire } from '@fortawesome/free-solid-svg-icons'
 
 const Title = () => {
 	return (
